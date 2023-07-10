9d95e051e3b80f71edc0d2ffb386884cd654ec27
onchainguy-eth <1436535+onchainguy-eth@users.noreply.github.com>
2023-07-10 14:11:55 +0200

    Fix syntax error

--- contracts/validator.sol
+++ contracts/validator.sol
@@ -1,13 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
 contract Validator {
     address public verifierContract;
     uint256 public merkleRoot;
 
-    constructor (address _verifierContract) {
+    constructor (address _verifierContract) public {
         verifierContract = _verifierContract;
         merkleRoot = 0;
     }
 
-    verify(bytes calldata proof, bytes calldata instances, uint256 calldata claimedMerkleRoot) {
+    function verify(bytes calldata proof, bytes calldata instances, uint256 claimedMerkleRoot) public {
         (bool success, ) = verifierContract.call(abi.encodePacked(instances, claimedMerkleRoot, proof));
         require(success, "proof did not verify!");
 
